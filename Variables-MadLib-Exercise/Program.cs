5e923bc4d56bc0fce5cb64591a1d3bd9e38dbaa1
Seth Bowman <brandonsethbowman@gmail.com>
2021-09-28 15:21:26 -0500

    Exercise created

--- Variables-MadLib-Exercise/Program.cs
+++ Variables-MadLib-Exercise/Program.cs
@@ -15,7 +15,7 @@ namespace Variables_MadLib_Exercise
             string relativesName;
             string noun;
             string typeOfLiquid;
-            string verEndingInING;
+            string verbEndingInING;
             string pluralPartOfTheBody;
             string pluralNoun;
             string secondVerbEndingInING;
@@ -23,10 +23,10 @@ namespace Variables_MadLib_Exercise
 
             // Give vaules to the variables above by taking input from the user. Example-- string variable name = Console.ReadLine();
             // Be sure to prompt the user before each value is assigned. Example -- Console.WriteLine("Please give me an adjective.");
+            
            
-
             // MadLib
-            Console.WriteLine($"It was a {adjective}, cold November day, I woke up to the {adjective2} smell of {typeOfBird} roasting in the {roomInAHouse} downstairs.\nI {pastTenseVerb} down the stairs to see if I could help {verb} the dinner.\nMy mom said, 'See if {relativesName} needs a fresh {noun}.'\nSo I carried a tray of glasses full of {typeOfLiquid} into the {verEndingInING} room.\nWhen I got there I couldn't believe my {pluralPartOfTheBody}! There were {pluralNoun} {secondVerbEndingInING} on the {noun2}!");
+            Console.WriteLine($"It was a {adjective}, cold November day, I woke up to the {adjective2} smell of {typeOfBird} roasting in the {roomInAHouse} downstairs.\nI {pastTenseVerb} down the stairs to see if I could help {verb} the dinner.\nMy mom said, 'See if {relativesName} needs a fresh {noun}.'\nSo I carried a tray of glasses full of {typeOfLiquid} into the {verbEndingInING} room.\nWhen I got there I couldn't believe my {pluralPartOfTheBody}! There were {pluralNoun} {secondVerbEndingInING} on the {noun2}!");
             
             // Run the application and see your story you've created
 
