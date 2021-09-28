1c8b216d847732b09061490847cafb00aa4c1f0b
Seth Bowman <brandonsethbowman@gmail.com>
2021-09-28 15:11:10 -0500

    Created exercise

---
+++ Variables-MadLib-Exercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Variables_MadLib_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adjective;
+            string adjective2;
+            string typeOfBird;
+            string roomInAHouse;
+            string pastTenseVerb;
+            string verb;
+            string relativesName;
+            string noun;
+            string typeOfLiquid;
+            string verEndingInING;
+            string pluralPartOfTheBody;
+            string pluralNoun;
+            string secondVerbEndingInING;
+            string noun2;
+
+            // Give vaules to the variables above by taking input from the user. Example-- string variable name = Console.ReadLine();
+            // Be sure to prompt the user before each value is assigned. Example -- Console.WriteLine("Please give me an adjective.");
+           
+
+            // MadLib
+            Console.WriteLine($"It was a {adjective}, cold November day, I woke up to the {adjective2} smell of {typeOfBird} roasting in the {roomInAHouse} downstairs.\nI {pastTenseVerb} down the stairs to see if I could help {verb} the dinner.\nMy mom said, 'See if {relativesName} needs a fresh {noun}.'\nSo I carried a tray of glasses full of {typeOfLiquid} into the {verEndingInING} room.\nWhen I got there I couldn't believe my {pluralPartOfTheBody}! There were {pluralNoun} {secondVerbEndingInING} on the {noun2}!");
+            
+            // Run the application and see your story you've created
+
+
+        }
+    }
+}
